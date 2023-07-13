3e5fa7c9b7d6daa8b3c4e381b06355ccdd40f508
MaragathaMeenakshi <113226919+MaragathaMeenakshi@users.noreply.github.com>
2023-07-14 01:28:38 +0530

    Update ssg.js

--- src/pages/ssg.js
+++ src/pages/ssg.js
@@ -12,25 +12,13 @@
 //     }
 // }
 
-// const userData = ({ user }) => {
-//     console.log(user);
-//     return (
-//         <>
-//             <div>Customers</div>
-//             <ul>
-//                 {Object.entries(user.allPosts).map((datas) => (
-//                     <li>
-//                         Customer name : {datas[1].data.name} <br></br>
-//                         Email : {datas[1].data.email} <br></br>
-//                         Phone Number : {datas[1].data.phonenumber}<br></br>
-//                         Office Address : {datas[1].data.officeaddress}<br></br>
-//                         Pincode : {datas[1].data.pincode}<br></br>
-//                         Website Url :{datas[1].data.websiteurl}<br></br>
-//                         <br></br>
-//                     </li>
-//                 ))}
-//             </ul>
-//         </>
-//     )
-// }
-// export default userData;
+const userData = ({ user }) => {
+    console.log(user);
+    return (
+        <>
+            <div>Customers</div>
+            
+        </>
+    )
+}
+export default userData;
