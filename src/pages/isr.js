1f477423a048a8b0a4dffb001b7a7cd07ebfa32a
MaragathaMeenakshi <113226919+MaragathaMeenakshi@users.noreply.github.com>
2023-07-14 01:28:16 +0530

    Update isr.js

--- src/pages/isr.js
+++ src/pages/isr.js
@@ -11,23 +11,10 @@
 //     };
 // }
 
-// const IncrementStaticGeneration = ({ user }) => {
-//     return (
-//         <> 
-//           <div>Customers</div>
-//             <ul>
-//                 {Object.entries(user.allPosts).map((datas) => (
-//                     <li>
-//                        Customer name : {datas[1].data.name} <br></br>
-//                         Email : {datas[1].data.email} <br></br>
-//                         Phone Number : {datas[1].data.phonenumber}<br></br>
-//                         Office Address : {datas[1].data.officeaddress}<br></br>
-//                         Pincode : {datas[1].data.pincode}<br></br>
-//                         Website Url :{datas[1].data.websiteurl}<br></br>
-//                         <br></br>
-//                     </li>
-//                 ))}
-//             </ul>
-//         </>
-//     )
-// }
+const IncrementStaticGeneration = () => {
+    return (
+        <> 
+          <div>Customers</div>
+        </>
+    )
+}
