37e14659a8e7def726c81619fb27584de7c4fe82
MaragathaMeenakshi <113226919+MaragathaMeenakshi@users.noreply.github.com>
2023-07-14 10:19:42 +0530

    Update isr.js

--- src/pages/isr.js
+++ src/pages/isr.js
@@ -1,20 +1,34 @@
-// export async function getStaticProps() {
-//     const response = await fetch('http://localhost:3000/api/user', {
-//         method: "GET"
-//     })
-//     const data = await response.json();
-//     return {
-//         props: {
-//             user: data,
-//         },
-//         revalidate :100,
-//     };
-// }
+export async function getStaticProps() {
+    const response = await fetch('process.env.client', {
+        method: "GET"
+    })
+    const data = await response.json();
+    return {
+        props: {
+            user: data,
+        },
+        revalidate :100,
+    };
+}
 
 const IncrementStaticGeneration = () => {
     return (
         <> 
           <div>Customers</div>
+        <ul>
+                {Object.entries(user.allPosts).map((datas) => (
+                    <li>
+                       Customer name : {datas[1].data.name} <br></br>
+                        Email : {datas[1].data.email} <br></br>
+                        Phone Number : {datas[1].data.phonenumber}<br></br>
+                        Office Address : {datas[1].data.officeaddress}<br></br>
+                        Pincode : {datas[1].data.pincode}<br></br>
+                        Website Url :{datas[1].data.websiteurl}<br></br>
+                        <br></br>
+                    </li>
+                ))}
+                
+            </ul>
         </>
     )
 }
