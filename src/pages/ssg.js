e4764c4a76116b37a2acb82d858d7b0f2dd7f29f
MaragathaMeenakshi <113226919+MaragathaMeenakshi@users.noreply.github.com>
2023-07-14 10:23:06 +0530

    Update ssg.js

--- src/pages/ssg.js
+++ src/pages/ssg.js
@@ -1,23 +1,35 @@
-// import { ListItem, ListItemText } from "@mui/material"
-// export async function getStaticProps() {
-//     const response = await fetch('http://localhost:3000/api/user', {
-//         method: "GET"
-//     })
-//     const data = await response.json();
-//     console.log(data);
-//     return {
-//         props: {
-//             user: data,
-//         }
-//     }
-// }
+import { ListItem, ListItemText } from "@mui/material"
+export async function getStaticProps() {
+    const response = await fetch('http://localhost:3000/api/user', {
+        method: "GET"
+    })
+    const data = await response.json();
+    console.log(data);
+    return {
+        props: {
+            user: data,
+        }
+    }
+}
 
 const userData = ({ user }) => {
     console.log(user);
     return (
         <>
             <div>Customers</div>
-            
+            <ul>
+                {Object.entries(user.allPosts).map((datas) => (
+                    <li>
+                        Customer name : {datas[1].data.name} <br></br>
+                        Email : {datas[1].data.email} <br></br>
+                        Phone Number : {datas[1].data.phonenumber}<br></br>
+                        Office Address : {datas[1].data.officeaddress}<br></br>
+                        Pincode : {datas[1].data.pincode}<br></br>
+                        Website Url :{datas[1].data.websiteurl}<br></br>
+                        <br></br>
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
